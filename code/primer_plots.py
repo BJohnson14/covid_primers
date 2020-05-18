dcd8433cd9b720f58e882c2b0dd47d654885927b
Casey Finnicum <caseyfinnicum@Caseys-MacBook-Pro.local>
2020-05-18 10:12:07 -0500

    add primer length analysis

--- code/primer_plots.py
+++ code/primer_plots.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def file_process(file, primer):
+def file_process(file, primer, target_length):
     df = pd.read_csv(file)
 
     # select the highest scoring alignment for all seqs queried
@@ -11,7 +11,10 @@ def file_process(file, primer):
                    'Minimum % identity': df1['%_identity'].min(), 'Max mismatches': df1['mismatches'].max(),
                    'Unique sequences': len(df1['subject acc.ver'].unique()), 'Total alignments': len(df1),
                    'Number of alignments with mismatches': len(df1.loc[df1['%_identity'] < 100, :]),
-                   'Proportion of alignments with 100% identity': 100 * (1 -len(df1.loc[df1['%_identity'] < 100, :]) / len(df1))}
+                   '% alignments with 100% identity': 100 * (1 -len(df1.loc[df1['%_identity'] < 100, :]) / len(df1)),
+                   'Mean Alignment Length': df1['alignment_length'].mean(),
+                   '% Sequences with Alignment Length Equal to full length of primer': (100 * (1 - len(df1.loc[df1['alignment_length'] < target_length, :]) / len(df1))),
+                   'Minimum Alignment Length': df1['alignment_length'].min()}
 
     # identify accession numbers for mismatches
     mismatch_seqs = df1.loc[df1['%_identity'] < 100, :]
@@ -20,15 +23,19 @@ def file_process(file, primer):
     df_out = pd.DataFrame.from_dict(output_dict, orient='index').T
     df_out['mismatch_accessions'] = [mismatch_list]
 
+    # identify accession numbers for short seqs
+    mismatch_seqs = df1.loc[df1['alignment_length'] < target_length, :]
+    print(mismatch_seqs)
+
     return df_out
 
 
-output_N1_forward = file_process('../data/BZSJCU50114-Alignment-HitTable_N1_Forward.csv', 'N1 Forward')
-output_N1_reverse = file_process('../data/BZT5F9NN114-Alignment-HitTable_N1_Reverse.csv', 'N1 Reverse')
-output_N1_probe = file_process('../data/BZTBUZH7114-Alignment-HitTable_N1_Probe.csv', 'N1 Probe')
-output_N2_forward = file_process('../data/BZTHE240114-Alignment-HitTable_N2_Forward.csv', 'N2 Forward')
-output_N2_reverse = file_process('../data/BZU005YT114-Alignment-HitTable_N2_Reverse.csv', 'N2 Reverse')
-output_N2_probe = file_process('../data/BZU4JNT2114-Alignment-HitTable_N2_Probe.csv', 'N2 Probe')
+output_N1_forward = file_process('../data/BZSJCU50114-Alignment-HitTable_N1_Forward.csv', 'N1 Forward', 20)
+output_N1_reverse = file_process('../data/BZT5F9NN114-Alignment-HitTable_N1_Reverse.csv', 'N1 Reverse', 24)
+output_N1_probe = file_process('../data/BZTBUZH7114-Alignment-HitTable_N1_Probe.csv', 'N1 Probe', 24)
+output_N2_forward = file_process('../data/BZTHE240114-Alignment-HitTable_N2_Forward.csv', 'N2 Forward', 20)
+output_N2_reverse = file_process('../data/BZU005YT114-Alignment-HitTable_N2_Reverse.csv', 'N2 Reverse', 18)
+output_N2_probe = file_process('../data/BZU4JNT2114-Alignment-HitTable_N2_Probe.csv', 'N2 Probe', 23)
 
 
 df_append = pd.concat([output_N1_forward, output_N1_reverse, output_N1_probe, output_N2_forward, output_N2_reverse,
